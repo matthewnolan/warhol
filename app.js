3a5f19eac2982ea6a3285c13d15345fb86f910f6
matthewnolan <matthewnolan415@gmail.com>
2016-07-07 05:45:51 -0400

    working gallery

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var auth = require('http-auth');
+var basic = auth.basic({
+	realm: "Simon Area.",
+	file: __dirname + "/users.htpasswd"
+});
+// un: andy  pw: warhol
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,8 +17,13 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+app.get('/admin', auth.connect(basic), function(request, response) {
+    response.render('index');
+});
+
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('owl');
 });
 
 app.listen(app.get('port'), function() {
